acdb5fd74af8237aee7478f74ac935a90131665c
IgorKilipenko <igorkili@mail.ru>
2016-12-10 09:07:25 +0700

    first test boostramp used

--- src/routes/contact/Contact.js
+++ src/routes/contact/Contact.js
@@ -10,13 +10,38 @@
 import React, { PropTypes } from 'react';
 import withStyles from 'isomorphic-style-loader/lib/withStyles';
 import s from './Contact.scss';
+import { Button } from 'react-bootstrap';
+import { ButtonToolbar } from 'react-bootstrap';
+
 
 function Contact({ title }) {
   return (
     <div className={s.root}>
       <div className={s.container}>
         <h1>{title}</h1>
-        <p>...</p>
+        <p>Мой тест11155gggg5555</p>
+        <ButtonToolbar>
+          {/* Standard button */}
+          <Button>Default</Button>
+
+          {/* Provides extra visual weight and identifies the primary action in a set of buttons */}
+          <Button bsStyle="primary">Primary</Button>
+
+          {/* Indicates a successful or positive action */}
+          <Button bsStyle="success">Success</Button>
+
+          {/* Contextual button for informational alert messages */}
+          <Button bsStyle="info">Info</Button>
+
+          {/* Indicates caution should be taken with this action */}
+          <Button bsStyle="warning">Warning</Button>
+
+          {/* Indicates a dangerous or potentially negative action */}
+          <Button bsStyle="danger">Danger</Button>
+
+          {/* Deemphasize a button by making it look like a link while maintaining button behavior */}
+          <Button bsStyle="link">Link</Button>
+        </ButtonToolbar>
       </div>
     </div>
   );
